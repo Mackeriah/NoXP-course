ab3cd1f7270ec105d5a8b47970ed32f32940fd2e
Owen <fitzgerald.owen@gmail.com>
2020-04-11 08:51:45 +1000

    after completing part 4

--- Assets/bulletBehaviour.cs
+++ Assets/bulletBehaviour.cs
@@ -20,4 +20,32 @@ public class bulletBehaviour : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter(Collision thisCollision)
+    {
+        // when we collide, we can ask the thing we collided with for a bunch of information (e.g. Type, Rigidbody)
+        // we're interested in the gameobject so we get more info on that specific object that we hit
+        // from this we can look for a specific Component (the ones added in Unity like Rigidbody, transform, material, mesh renderer etc)
+        // and we can specify the game component we're interested in which is the Class enemyBehaviour
+        // this is all so we can determine whether the thing we hit, is an enemy
+
+        /*
+         // this was the first version, but Tom then created a new variable for thisCollision.gameObject to make it easier to read
+        if (thisCollision.gameObject.GetComponent<enemyBehaviour>() != null)
+        {
+            Destroy(thisCollision.gameObject);  // destroy what we collided with
+            Destroy(gameObject);  // destroy ourselves
+        }
+        */
+
+        GameObject theirGameObject = thisCollision.gameObject;
+
+        if (theirGameObject.GetComponent<enemyBehaviour>() != null)
+        {
+            Destroy(theirGameObject);  // destroy what we collided with
+            Destroy(gameObject);  // destroy ourselves
+        }
+
+
+    }
 }
---
+++ Assets/enemyBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBehaviour : MonoBehaviour
+{
+    // Track the player's position
+    public GameObject player;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ensure enemy physics will work
+        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+
+        // Calculate direction and distance to travel to the player        
+        Vector3 vectorToPlayer = player.transform.position - transform.position;
+        // Use this as our velocity but normalize the value to 1 metre and then multiply by our speed
+        // (Velocity in a Physics sense is direction * speed)
+        ourRigidBody.velocity = vectorToPlayer.normalized * speed;
+
+    }
+}
