35021db51fe61a82491a63f8f498f897fffccafb
Owen <fitzgerald.owen@gmail.com>
2020-04-19 12:57:39 +1000

    part 5 after reverted original issue and recoded

--- Assets/PlayerBehaviour.cs
+++ Assets/PlayerBehaviour.cs
@@ -21,16 +21,21 @@ public class PlayerBehaviour : MonoBehaviour
 
         // Find the new position we'll move to
         Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));  // direction player trying to move (x,y,z but we don't care about y so zero)
-
+        
         // Ensure player physics will work
         Rigidbody ourRigidBody = GetComponent<Rigidbody>();
-
+        
         // Set our velocity
         // (Velocity in a Physics sense is direction * speed)
         ourRigidBody.velocity = inputVector * speed;
-
+        
+        Ray rayFromCameraToCursor = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+        playerPlane.Raycast(rayFromCameraToCursor, out float distanceFromCamera);
+        Vector3 cursorPosition = rayFromCameraToCursor.GetPoint(distanceFromCamera);
+        
         // Face the position the player is moving controller
-        Vector3 lookAtPosition = transform.position + inputVector;
+        Vector3 lookAtPosition = cursorPosition;
         transform.LookAt(lookAtPosition);
         
         // If clicked, create a bullet at our current position
