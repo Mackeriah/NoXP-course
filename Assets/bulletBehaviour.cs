09666092662da7b3f711a7a3641f28d5d3b004ac
Owen <fitzgerald.owen@gmail.com>
2020-04-24 20:55:43 +1000

    final commit for lesson 5

--- Assets/bulletBehaviour.cs
+++ Assets/bulletBehaviour.cs
@@ -25,11 +25,12 @@ public class bulletBehaviour : MonoBehaviour
         secondsUntilDestroyed -= Time.deltaTime;
 
         // a fun way to make the bullets enlarge and pop out of existance!
-        /*if (secondsUntilDestroyed < 1)
+       if (secondsUntilDestroyed < 1)
         {
             transform.localScale = Vector3.one * secondsUntilDestroyed;
-        }*/
+        }
 
+/*
         if (secondsUntilDestroyed < 1)
         {
             transform.localScale *= secondsUntilDestroyed;
@@ -39,7 +40,7 @@ public class bulletBehaviour : MonoBehaviour
         if (secondsUntilDestroyed < 0)
         {
             Destroy(gameObject);
-        }
+        }*/
         
     }
 
