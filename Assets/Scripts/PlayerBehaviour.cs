5a61e8e2f48bad99c486213723cddbb7e5b74760
Owen <fitzgerald.owen@gmail.com>
2020-07-09 07:09:28 +1000

    19 minutes into episode 8: created weapon class

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Never set the value of a public variable in code - a different value in the inspector will override the code without telling you
-    // If you need to, set it in Start() instead
     public float speed;
-    public GameObject bulletPrefab;
+    public WeaponBehaviour myWeapon;
 
     public float secondsBetweenShots;   // assigned in Inspector
     private float secondsSinceLastShot;
@@ -41,16 +39,13 @@ public class PlayerBehaviour : MonoBehaviour
         Vector3 lookAtPosition = cursorPosition;
         transform.LookAt(lookAtPosition);
 
-        // track time since last shot
-        secondsSinceLastShot += Time.deltaTime;
-
-        // If clicked, create a bullet at our current position
-        if (secondsSinceLastShot >= secondsBetweenShots && Input.GetButton("Fire1")) 
+        
+        // Firing
+        if (Input.GetButton("Fire1")) 
         {
-            // Instantiate is a function and the stuff after are arguments
-            Instantiate(bulletPrefab, transform.position + transform.forward, transform.rotation);
-            secondsSinceLastShot = 0;
-        }       
+            // Tell our weapon to fire (using the Class "Fire" which is assigned to the weapon)
+            myWeapon.Fire();
+        } 
  
 
     }
---
+++ Assets/WeaponBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehaviour : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float secondsBetweenShots;
+    private float secondsSinceLastShot;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // reset firing timing
+        secondsSinceLastShot = secondsBetweenShots;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Track how long since last shot
+        secondsSinceLastShot += Time.deltaTime;
+    }
+
+    public void Fire()  // this is our weapon class
+    {
+        // Check enough time has passed
+        if (secondsSinceLastShot >= secondsBetweenShots)
+        {
+            // Create bullet
+            Instantiate(bulletPrefab, transform.position + transform.forward, transform.rotation);
+            // Reset timer
+            secondsSinceLastShot = 0;
+        }
+    }
+
+}
