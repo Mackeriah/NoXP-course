447e108f35f230ed7e214a6a65f66e7d0bae9d26
Owen <fitzgerald.owen@gmail.com>
2020-07-23 20:36:22 +1000

    item pickup added (for learning)

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,6 +7,7 @@ public class PlayerBehaviour : MonoBehaviour
     public float speed;
     public List<WeaponBehaviour> weaponList = new List<WeaponBehaviour>();
     public int selectedWeaponIndex;
+    public int itemsCollected;
 
 
     void Start()
@@ -21,7 +22,7 @@ public class PlayerBehaviour : MonoBehaviour
         // Find the new position we'll move to
         Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));  // direction player trying to move (x,y,z but we don't care about y so zero)
         
-        // Ensure player physics will work
+        // Ensure player physics will work (future Owen:  hang on, isn't THIS bit simply storing 
         Rigidbody ourRigidBody = GetComponent<Rigidbody>();
         
         // Set our velocity
@@ -92,6 +93,17 @@ public class PlayerBehaviour : MonoBehaviour
             ChangeWeaponIndex(weaponList.Count - 1);  // select the weapon
         }
 
+    }
+
+    private void OnCollisionEnter(Collision thisCollision)  // When the player collides with another GameObject
+    {
+        GameObject theirGameObject = thisCollision.gameObject;  // store that GameObject in the variable theirGameObject 
+
+        if (theirGameObject.GetComponent<Collectible>() != null)  // if the thing we hit has a Collectible component
+        {
+            itemsCollected += 1;
+            Destroy(theirGameObject);            
+        }
     }
 
 
